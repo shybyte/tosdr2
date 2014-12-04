9a6804913dfad2acc8e34f936dc5205bd42043a5
shybyte <ein@volloeko.de>
2014-12-04 13:31:37 +0100

    Add Topics Selector to Submit-Points

--- client/submit-point/submit-point.js
+++ client/submit-point/submit-point.js
@@ -1,9 +1,14 @@
 Template.submitPoint.helpers({
   services: function () {
-      return Services.find()
+    return Services.find();
+  },
+  topics: function () {
+    return Topics.find();
   }
+
 });
 
 Template.submitPoint.rendered = function () {
   $("#services").select2();
+  $("#topics").select2();
 };
