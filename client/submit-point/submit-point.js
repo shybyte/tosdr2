2eb9b26b638196397759a89a339cdaa1426f1f93
shybyte <ein@volloeko.de>
2014-12-04 14:26:43 +0100

    Save submitted point

--- client/submit-point/submit-point.js
+++ client/submit-point/submit-point.js
@@ -9,6 +9,28 @@ Template.submitPoint.helpers({
 });
 
 Template.submitPoint.rendered = function () {
-  $("#services").select2();
-  $("#topics").select2();
+  $("#services").select2({placeholder: "Select at least one service"});
+  $("#topics").select2({placeholder: "Select at least one topic"});
 };
+
+Template.submitPoint.events({
+  'submit form': function (event,template) {
+    event.preventDefault();
+    // TODO: Here I have a lots of questions
+    var newPoint = {
+      services: $(template.find('#services')).val(),
+      title: template.find('#titleField').value,
+      topics: $(template.find('#topics')).val(),
+      tosdr: {
+        binding: true, // TODO: Set point binding, but how?
+        point: $('input[name="point"]:checked').val(),
+        tldr: template.find('#summaryField').value,
+        sources: [template.find('#sourceField').value]
+      }
+    };
+    console.log('Save new point:', newPoint);
+    Points.insert(newPoint);
+    Router.go('index');
+  }
+})
+
