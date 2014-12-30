ef49b832c0c7a88450beb3691b491e28726be657
shybyte <ein@volloeko.de>
2014-12-30 20:01:08 +0100

    Added Approved Filter to Moderate Points Page

--- client/points/moderate/moderate-points.js
+++ client/points/moderate/moderate-points.js
@@ -1,3 +1,25 @@
+var _ = lodash;
+
+Template.moderatePoints.helpers({
+  ifQueryParameterThenSelected: function (key, value) {
+    return (pointRoutesUtils.getFilter()[key] === value) ? 'selected' : '';
+  },
+  query: function () {
+    return pointRoutesUtils.getFilter();
+  }
+});
+
+Template.moderatePoints.events({
+  'change #approvedFilter': function (event, template) {
+    var oldFilter = pointRoutesUtils.getFilter();
+    var newApprovedFilterValue = $(event.target).val();
+    var newFilter = _.assign({}, oldFilter, {
+      approved: newApprovedFilterValue
+    });
+    Router.go('moderatePoints', {}, {query: newFilter});
+  }
+});
+
 Template.moderatePoint.events({
   'click .approveButton': function () {
     Points.update(this._id, {
